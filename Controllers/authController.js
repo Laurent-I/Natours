cdb75e583f09a5c8f5c5010c2d2a1f445c728209
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-25 01:43:41 +0200

    Authorizing only the admin and lead guide to delete a tour

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -49,7 +49,6 @@ exports.login =catchAsync(async(req, res, next) => {
         token,
     })
 });
-
 exports.protect = catchAsync(async (req, res, next) => {
     //1) Getting the token and check if its there
     let token
@@ -80,3 +79,14 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser
     next();
 })
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)){
+            return next(
+                new AppError('You do not have permission to perform this action', 403)
+            )
+        }
+        next();
+    }
+}
